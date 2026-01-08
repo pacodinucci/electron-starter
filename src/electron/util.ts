099c1fb13f42d1b9aa9e377b49c09ed7884c8ac5
pacodinucci <franciscoldinucci@gmail.com>
2026-01-08 11:33:08 -0300

    types electron update

--- src/electron/util.ts
+++ src/electron/util.ts
@@ -1,12 +1,20 @@
-import { ipcMain } from "electron";
+import { ipcMain, WebContents } from "electron";
 
 export function isDev(): boolean {
   return process.env.NODE_ENV === "development";
 }
 
-export function ipcHandle<Key extends keyof EventPayloadMapping>(
+export function ipcMainHandle<Key extends keyof EventPayloadMapping>(
   key: Key,
   handler: () => EventPayloadMapping[Key]
 ) {
   ipcMain.handle(key, () => handler());
+}
+
+export function ipcWebContentsSend<Key extends keyof EventPayloadMapping>(
+  key: Key,
+  webContents: WebContents,
+  payload: EventPayloadMapping[Key]
+) {
+  webContents.send(key, payload);
 }
